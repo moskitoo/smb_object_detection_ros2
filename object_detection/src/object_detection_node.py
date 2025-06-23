3fa41f9c3830778ff3d7edd173adcb8a8e2a8c2d
moskit <tomek_niedzi@o2.pl>
2025-06-23 14:34:44 +0000

    modified markers

--- object_detection/src/object_detection_node.py
+++ object_detection/src/object_detection_node.py
@@ -463,13 +463,13 @@ class ObjectDetectionNode(Node):
             marker.pose.orientation.y = 0.0
             marker.pose.orientation.z = 0.0
             marker.pose.orientation.w = 1.0
-            marker.scale.x = 0.3
-            marker.scale.y = 0.3
-            marker.scale.z = 0.3
+            marker.scale.x = 0.5
+            marker.scale.y = 0.5
+            marker.scale.z = 0.5
             marker.color.a = 1.0  # Fully opaque
-            marker.color.r = 1.0
+            marker.color.r = 0.0
             marker.color.g = 0.0
-            marker.color.b = 0.0
+            marker.color.b = 1.0
 
             self.marker_pub.publish(marker)
 
